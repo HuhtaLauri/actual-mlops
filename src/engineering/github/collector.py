13c2c3d1c2d5245ef4750db0757fed1fd33587e5
Lauri <huhta.lauri@gmail.com>
2024-09-01 15:47:50 +0300

    Remove lakefs references from collector

--- src/engineering/github/collector.py
+++ src/engineering/github/collector.py
@@ -6,8 +6,6 @@ import os
 import json
 from typing import Dict, Any
 from enum import Enum
-import lakefs
-from lakefs import Client
 from datetime import datetime, timedelta
 from src.paths import GITHUB_RAW_DATA_DIR, GITHUB_ROOT_PATH, GITHUB_COMMITS_RAW_DIR_PATH, GITHUB_REPOSITORIES_RAW_DIR_PATH
 import shutil
@@ -21,19 +19,6 @@ if not CURRENT_BRANCH:
 load_dotenv()
 
 API_BASEURL = "https://api.github.com"
-LAKEFS_REPO = os.environ.get("LAKEFS_REPO")
-if not LAKEFS_REPO:
-    raise ValueError("Must set 'LAKEFS_REPO' environment variable")
-
-
-clt = Client(
-    host=os.environ.get("LAKEFS_HOST", "localhost"),
-    username=os.environ.get("LAKEFS_USERNAME", "lakefs"),
-    password=os.environ.get("LAKEFS_PASSWORD", "lakefs")
-)
-
-REPO = lakefs.Repository(LAKEFS_REPO)
-BRANCH = REPO.branch(CURRENT_BRANCH)
 
 class WriteMode(Enum):
     WRITE = "w"
@@ -43,10 +28,8 @@ class WriteMode(Enum):
 
 @click.group()
 @click.pass_context
-@click.option("lakefs_enabled", "--lake", is_flag=True, default=False)
-def main(ctx, lakefs_enabled):
+def main(ctx):
     ctx.ensure_object(dict)
-    ctx.obj["lakefs_enabled"] = lakefs_enabled
     os.makedirs("data/raw/", exist_ok = True)
 
 
@@ -79,12 +62,6 @@ def _write_dict_data(result: Dict[str, Any],
         out_file.write("\n")
         
 
-def upload_to_lake(path: Path, lake_path: Path):
-    if path.suffix == ".json":
-        obj = BRANCH.object(path=str(lake_path)).upload(content_type="application/json", data=open(path).read())
-        return obj
-    return None
-
 
 @main.command
 @click.option("owner", "--owner", "-o", required=True)
@@ -94,7 +71,6 @@ def collect_repository(owner: str, repo: str):
     destination_file = Path(os.path.join(GITHUB_REPOSITORIES_RAW_DIR_PATH, "repositories.json"))
     write_result_to_disk(resp, destination_file, mode=WriteMode.APPEND)
 
-    upload_to_lake(destination_file, destination_file.relative_to(GITHUB_ROOT_PATH))
 
 
 def _collect_repository(owner: str, repo: str) -> Dict[str, Any]:
@@ -155,9 +131,6 @@ def _collect_commits(ctx, owner: str, repo: str, start_date: datetime, end_date:
             destination=destination_file,
             mode=WriteMode.APPEND
         )
-    
-    if ctx.obj["lakefs_enabled"]:
-        upload_to_lake(destination_file, destination_file.relative_to(GITHUB_ROOT_PATH))
 
 
 
