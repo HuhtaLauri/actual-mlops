d44878a13cfecbe9eb12a01b1c88231f25820335
Lauri <huhta.lauri@gmail.com>
2024-10-01 19:17:05 +0300

    brr

--- src/paths.py
+++ src/paths.py
@@ -2,10 +2,10 @@ from pathlib import Path
 import os
 
 
-GITHUB_ROOT_PATH = Path(os.path.join("src", "engineering", "github"))
+GITHUB_ROOT_PATH = Path("data")
 
 # RAW
-GITHUB_RAW_DATA_DIR = Path(os.path.join(GITHUB_ROOT_PATH, "data", "raw"))
+GITHUB_RAW_DATA_DIR = Path(os.path.join(GITHUB_ROOT_PATH, "raw"))
 GITHUB_COMMITS_RAW_DIR_PATH = Path(os.path.join(GITHUB_RAW_DATA_DIR, "commits"))
 GITHUB_REPOSITORIES_RAW_DIR_PATH = Path(
     os.path.join(GITHUB_RAW_DATA_DIR, "repositories")
